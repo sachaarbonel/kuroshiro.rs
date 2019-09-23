95bebda167d7fb2a42d6d7e7b9ae99bb90435ff8
Sach97 <sacha.arbonel@hotmail.fr>
2019-09-23 09:43:01 +0200

    tokenize kanji to kanas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use kuromoji::Tokenizer;
+
 fn main() {
-    println!("Hello, world!");
+    let mut tokenizer = Tokenizer::normal();
+    let tokens = tokenizer.tokenize(
+        "本項で解説する地方病とは、山梨県における日本住血吸虫症の呼称であり、\
+         長い間その原因が明らかにならず住民を苦しめた感染症である。",
+    );
+    for token in tokens {
+        println!("text {:?} reading {:?} ", token.text, token.detail.reading);
+    }
 }
