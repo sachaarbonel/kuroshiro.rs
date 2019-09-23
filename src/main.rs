740ca6038a14d94758e311fcb5988b3106340bce
Sach97 <sacha.arbonel@hotmail.fr>
2019-09-23 10:45:54 +0200

    basic converter

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
 use kuromoji::Tokenizer;
 
+struct Kuroshiro;
+impl Kuroshiro {
+    fn convert(text: &str) -> String {
+        let mut tokenizer = Tokenizer::normal();
+        let tokens = tokenizer.tokenize(text);
+        let parsed_tokens = tokens
+            .as_slice()
+            .iter()
+            .map(|token| token.detail.reading.to_owned()) //Token::new(token.clone().text, &token.clone().detail.reading)
+            .collect::<Vec<String>>(); //.collect::<Vec<Token>>();
+        let converted_tokens = parsed_tokens.join("");
+        converted_tokens
+    }
+}
+
+// struct Token<'a> {
+//     text: &'a str,
+//     reading: String,
+// }
+
+// impl<'a> Token<'a> {
+//     fn new(text: &'a str, reading: &String) -> Token<'a> {
+//         Token {
+//             text: text,
+//             reading: reading.to_owned(),
+//         }
+//     }
+// }
 fn main() {
-    let mut tokenizer = Tokenizer::normal();
-    let tokens = tokenizer.tokenize(
+    let romaji = Kuroshiro::convert(
         "本項で解説する地方病とは、山梨県における日本住血吸虫症の呼称であり、\
          長い間その原因が明らかにならず住民を苦しめた感染症である。",
     );
-    for token in tokens {
-        println!("text {:?} reading {:?} ", token.text, token.detail.reading);
-    }
+    println!("{}", romaji);
 }
